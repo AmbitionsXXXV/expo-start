6fef7690725a2d32a6338e549a5b345dd2929fd8
etcetera <yilonglei790@gmail.com>
2025-05-14 16:46:47 +0800

    feat: implement dark mode toggle and update asset paths for better organization

--- apps/native/app/(tabs)/about.tsx
+++ apps/native/app/(tabs)/about.tsx
@@ -1,5 +1,19 @@
-import { Text } from "react-native";
+import { useColorScheme } from "@/lib/use-color-scheme";
+import { Button } from "react-native";
 
 export default function About() {
-	return <Text>About</Text>;
+	const { setColorScheme, isDarkColorScheme } = useColorScheme();
+
+	return (
+		<Button
+			title={isDarkColorScheme ? "Set to light" : "Set to dark"}
+			onPress={() => {
+				if (isDarkColorScheme) {
+					setColorScheme("light");
+				} else {
+					setColorScheme("dark");
+				}
+			}}
+		/>
+	);
 }
