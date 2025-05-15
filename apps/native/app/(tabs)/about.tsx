ec31eb015b20bf45b209481c1905064d9bb7b8c7
etcetera <yilonglei790@gmail.com>
2025-05-15 22:22:28 +0800

    feat: ✨ integrate expo-crypto for hashing functionality and update app configuration for expo-dev-client

--- apps/native/app/(tabs)/about.tsx
+++ apps/native/app/(tabs)/about.tsx
@@ -1,19 +1,43 @@
 import { useColorScheme } from "@/lib/use-color-scheme";
-import { Button } from "react-native";
+import { useEffect } from "react";
+import { Button, View } from "react-native";
+
+import * as Crypto from "expo-crypto";
+import * as DevClient from "expo-dev-client";
 
 export default function About() {
 	const { setColorScheme, isDarkColorScheme } = useColorScheme();
 
+	useEffect(() => {
+		(async () => {
+			const digest = await Crypto.digestStringAsync(
+				Crypto.CryptoDigestAlgorithm.SHA256,
+				"GitHub stars are neat 🌟",
+			);
+			console.log("Digest: ", digest);
+			/* Some crypto operation... */
+		})();
+	}, []);
+
 	return (
-		<Button
-			title={isDarkColorScheme ? "Set to light" : "Set to dark"}
-			onPress={() => {
-				if (isDarkColorScheme) {
-					setColorScheme("light");
-				} else {
-					setColorScheme("dark");
-				}
-			}}
-		/>
+		<View className="flex gap-4">
+			<Button
+				title={isDarkColorScheme ? "Set to light" : "Set to dark"}
+				onPress={() => {
+					if (isDarkColorScheme) {
+						setColorScheme("light");
+					} else {
+						setColorScheme("dark");
+					}
+				}}
+			/>
+
+			<Button
+				title="Dev menu"
+				onPress={() => {
+					DevClient.openMenu();
+				}}
+			/>
+		</View>
 	);
 }
