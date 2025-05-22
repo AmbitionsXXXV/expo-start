afa06376d09024ea8f1cf019f8c310113e9ddad9
etcetera <yilonglei790@gmail.com>
2025-05-23 00:22:32 +0800

    feat: ✨ refactor app configuration by consolidating settings into app.config.ts and removing app.json

--- apps/native/app.config.ts
+++ apps/native/app.config.ts
@@ -1,8 +1,58 @@
-import type { ConfigContext, ExpoConfig } from "expo/config";
+import type { ExpoConfig } from "expo/config";
 
-export default ({ config }: ConfigContext): ExpoConfig => ({
-	...config,
-	slug: "my-app",
-	name: "My App",
-	plugins: [...(config.plugins || []), "expo-localization"],
-});
+export default {
+	expo: {
+		name: "my-better-t-app",
+		slug: "my-better-t-app",
+		version: "1.0.0",
+		scheme: "my-better-t-app",
+		web: {
+			bundler: "metro",
+			output: "static",
+			favicon: "./assets/images/favicon.png",
+		},
+		plugins: [
+			"expo-router",
+			"expo-secure-store",
+			"expo-web-browser",
+			[
+				"expo-splash-screen",
+				{
+					image: "./assets/images/splash-icon.png",
+				},
+			],
+			[
+				"expo-dev-client",
+				{
+					launchMode: "most-recent",
+				},
+			],
+		],
+		experiments: {
+			typedRoutes: true,
+			tsconfigPaths: true,
+		},
+		newArchEnabled: true,
+		orientation: "portrait",
+		icon: "./assets/images/icon.png",
+		userInterfaceStyle: "automatic",
+		splash: {
+			image: "./assets/images/splash.png",
+			resizeMode: "contain",
+			backgroundColor: "#ffffff",
+		},
+		assetBundlePatterns: ["**/*"],
+		ios: {
+			supportsTablet: true,
+			bundleIdentifier: "com.amanvarshney01.mybettertapp",
+		},
+		android: {
+			adaptiveIcon: {
+				foregroundImage: "./assets/images/adaptive-icon.png",
+				backgroundColor: "#ffffff",
+			},
+			package: "com.amanvarshney01.mybettertapp",
+			edgeToEdgeEnabled: true,
+		},
+	},
+} as { expo: ExpoConfig };
